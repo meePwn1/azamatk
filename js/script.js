121eb568e411de66dac7e3840fe7c88d80743fe2
Azamat <azamatk9799@gmail.com>
2023-04-08 06:09:01 +0500

    commit#

---
+++ js/script.js
@@ -0,0 +1,49 @@
+import { Popup } from "./popup.js";
+
+let popupTitle = null;
+let popupImage = null;
+
+
+window.addEventListener('load', documentLoaded);
+
+function documentLoaded(e) {
+	const popup = new Popup();
+	popupTitle = document.querySelector('.popup__title');
+	popupImage = document.querySelector('.popup__image img');
+
+	document.addEventListener('click', actionDocument);
+}
+
+function actionDocument(e) {
+	let target = e.target;
+
+	// Установка изображения для попапа
+
+	if (target.closest('.works__img') && target.closest('.works__img').hasAttribute('data-src-image')) {
+		const imageUrl = target.closest('.works__img').dataset.srcImage;
+		const titleText = target.closest('.works__item').querySelector('.works__title').innerHTML;
+
+		popupTitle.innerHTML = titleText;
+		popupImage.src = imageUrl;
+	}
+
+	// Настройка фильтра
+	if (target.classList.contains("filter__button") && !target.classList.contains("active")) {
+		const activeElement = document.querySelector('.filter__button.active');
+		const elements = document.querySelectorAll('.works__item');
+		const elementType = target.dataset.workType;
+
+		activeElement.classList.remove("active");
+		target.classList.add("active");
+
+		elements.forEach(element => {
+			if (element.dataset.workType && elementType !== "All") {
+				let arr = element.dataset.workType.split(',');
+				!elementType || !arr.includes(elementType) ?
+					element.hidden = true : element.hidden = false
+			} else {
+				element.hidden = false
+			}
+		})
+	}
+}
